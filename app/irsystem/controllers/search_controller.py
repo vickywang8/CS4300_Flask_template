7fd1962f924cb11af6a052a01463f51fb6a2e907
priyankarathnam <pcr43@cornell.edu>
2018-04-11 17:07:54 -0400

    results in table format now

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -27,29 +27,26 @@ def search():
 		search_endpoint = "{}events.json?classificationName=music&city={}&countryCode=US&startDateTime={}{}".format(TICKETMASTER_API_URL, queried_location, queried_date, TICKETMASTER_API_KEY)
 		search_response = requests.get(search_endpoint)
 		search_data = json.loads(search_response.text)
-		data = search_data
-		events = data["_embedded"]["events"]
-		concerts_dict = {}
-		for event in events:
-			genre = event["classifications"][0]["genre"]["name"]
-			if queried_genre.lower() in genre.lower():
-				name = event["name"]
-				date = event["dates"]["start"]["localDate"]
-				try:
-					time = event["dates"]["start"]["localTime"]
-				except:
-					time = ""
-				link = event["url"]
-				location = event["_embedded"]["venues"][0]["name"]
-				concert = {"date": date, "time": time, "link": link, "location": location}
-				if name not in concerts_dict:
-					concerts_dict[name] = [concert]
-				else:
-					concerts_dict[name].append(concert)
-
-		for concert in concerts_dict.keys():
-			print concert
+		data = get_concert_data(search_data, queried_genre)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
+def get_concert_data(search_data, queried_genre):
+	events = search_data["_embedded"]["events"]
+	concerts = []
+	for event in events:
+		genre = event["classifications"][0]["genre"]["name"]
+		if queried_genre.lower() in genre.lower():
+			name = event["name"]
+			date = event["dates"]["start"]["localDate"]
+			try:
+				time = event["dates"]["start"]["localTime"]
+			except:
+				time = ""
+			link = event["url"]
+			location = event["_embedded"]["venues"][0]["name"]
+			concert_data = {"name": name, "date": date, "time": time, "link": link, "location": location}
+			concerts.append(concert_data)
+	print(concerts[1])
+	return concerts
 
 
