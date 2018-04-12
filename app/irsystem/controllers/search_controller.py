edc84e3463180696912a70e0f691a93f141b6bc6
priyankarathnam <pcr43@cornell.edu>
2018-04-12 11:09:08 -0400

    added date range picker and query to stay in search

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -5,6 +5,7 @@ import json
 from flask import Flask, request, redirect, g, render_template
 import requests
 import json
+import dateutil.parser as dparser
 
 project_name = "Ilan's Cool Project Template"
 net_id = "Ilan Filonenko: if56"
@@ -18,34 +19,59 @@ def search():
 	queried_location = request.args.get('location')
 	queried_date = request.args.get('date')
 	if not (queried_genre or queried_location or queried_date):
-		print("blank")
 		data = []
 		output_message = ''
 	else:
-		output_message = "You're looking for " + queried_genre + " concerts in " + queried_location + " on " + queried_date
-		# data = range(5)
-		search_endpoint = "{}events.json?classificationName=music&city={}&countryCode=US&startDateTime={}{}".format(TICKETMASTER_API_URL, queried_location, queried_date, TICKETMASTER_API_KEY)
+		if not queried_genre:
+			queried_genre = "music"
+		if queried_date:
+			start_date, end_date = format_date(queried_date)
+		output_message = format_output_message(queried_genre, queried_location, queried_date)
+		search_endpoint = "{}events.json?classificationName={}&city={}&countryCode=US&startDateTime={}&endDateTime={}{}".format(TICKETMASTER_API_URL, queried_genre, queried_location, start_date, end_date, TICKETMASTER_API_KEY)
 		search_response = requests.get(search_endpoint)
 		search_data = json.loads(search_response.text)
-		data = get_concert_data(search_data, queried_genre)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+		data = get_concert_data(search_data)
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data, queried_genre= queried_genre, queried_date=queried_date, queried_location=queried_location)
 
-def get_concert_data(search_data, queried_genre):
+def get_concert_data(search_data):
 	events = search_data["_embedded"]["events"]
 	concerts = []
 	for event in events:
-		genre = event["classifications"][0]["genre"]["name"]
-		if queried_genre.lower() in genre.lower():
-			name = event["name"]
-			date = event["dates"]["start"]["localDate"]
-			try:
-				time = event["dates"]["start"]["localTime"]
-			except:
-				time = ""
-			link = event["url"]
-			location = event["_embedded"]["venues"][0]["name"]
-			concert_data = {"name": name, "date": date, "time": time, "link": link, "location": location}
-			concerts.append(concert_data)
+		# genre = event["classifications"][0]["genre"]["name"]
+		# if queried_genre.lower() in genre.lower():
+		name = event["name"]
+		date = event["dates"]["start"]["localDate"]
+		try:
+			time = event["dates"]["start"]["localTime"]
+		except:
+			time = ""
+		link = event["url"]
+		location = event["_embedded"]["venues"][0]["name"]
+		concert_data = {"name": name, "date": date, "time": time, "link": link, "location": location}
+		concerts.append(concert_data)
 	return concerts
 
+def format_date(queried_date):
+	start_date = queried_date.split("-")[0]
+	end_date = queried_date.split("-")[1]
+	start_date= str(dparser.parse(start_date))
+	start_date = start_date.replace(" ", "T")+"Z"
+	end_date = str(dparser.parse(end_date))
+	end_date = end_date.replace(" ", "T")+"Z"
+	return start_date, end_date
+
+def format_output_message(queried_genre, queried_location, queried_date):
+	if queried_genre == "music":
+		queried_genre = ""
+	else:
+		queried_genre = queried_genre + " "
+	if queried_location:
+		queried_location = "in " + queried_location + " "
+	else:
+		queried_location = ""
+	if queried_date:
+		queried_date = "on " + queried_date
+	else:
+		queried_date = ""
+	return "You're looking for " + queried_genre + "concerts " + queried_location + queried_date
 
