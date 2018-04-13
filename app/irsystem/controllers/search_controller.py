6cb2c38bf0dea8e5810e67dec08741678c7247c8
priyankarathnam <pcr43@cornell.edu>
2018-04-12 22:36:00 -0400

    added images to table

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -88,7 +88,8 @@ def get_concert_data(search_data):
 		link = event["url"]
 		venue = event["_embedded"]["venues"][0]["name"]
 		city = event["_embedded"]["venues"][0]["city"]["name"]
-		concert_data = {"name": name, "date": date, "time": time, "link": link, "venue": venue, "city": city}
+		image = event["images"][0]["url"]
+		concert_data = {"name": name, "date": date, "time": time, "link": link, "venue": venue, "city": city, "image": image}
 		concerts.append(concert_data)
 	return concerts
 
