a84f0cde471482d9cc46c39c9ca8bdcade765c42
Emily S <eys27@cornell.edu>
2018-04-15 13:38:37 -0400

    copied code over from a4

--- cos_sim.py
+++ cos_sim.py
@@ -1,8 +1,213 @@
 import numpy as np
 import math
+import csv
+from collections import Counter
 from collections import defaultdict
 from nltk.tokenize import TreebankWordTokenizer
 
-tokenizer = TreebankWordTokenizer()
-tokenize_blurb = tokenizer.tokenize(blurb)
+
+def build_inverted_index(msgs):
+    """ Builds an inverted index from the messages.
+    
+    Arguments
+    =========
+    
+    msgs: list of dicts.
+        Each message in this list already has a 'toks'
+        field that contains the tokenized message.
+    
+    Returns
+    =======
+    
+    index: dict
+        For each term, the index contains a list of
+        tuples (doc_id, count_of_term_in_doc):
+        index[term] = [(d1, tf1), (d2, tf2), ...]
+        
+    Example
+    =======
+    
+    >> test_idx = build_inverted_index([
+    ...    {'toks': ['to', 'be', 'or', 'not', 'to', 'be']},
+    ...    {'toks': ['do', 'be', 'do', 'be', 'do']}])
+    
+    >> test_idx['be']
+    [(0, 2), (1, 2)]
+    
+    >> test_idx['not']
+    [(0, 1)]
+    
+    """
+    # YOUR CODE HERE
+    index = defaultdict(list)
+    
+    for i in range(0, len(msgs)):
+        
+        # Counter to count all occurences of word in tokenized message
+        counts = Counter(msgs[i]['blurb'])
+        
+        # Add to dictionary
+        for word in counts:
+            index[word].append((i, counts[word]))
+            
+    return index
+
+
+def compute_idf(inv_idx, n_docs, min_df=10, max_df_ratio=0.95):
+    """ Compute term IDF values from the inverted index.
+    
+    Words that are too frequent or too infrequent get pruned.
+    
+    
+    Arguments
+    =========
+    
+    inv_idx: an inverted index as above
+    
+    n_docs: int,
+        The number of documents.
+        
+    min_df: int,
+        Minimum number of documents a term must occur in.
+        Less frequent words get ignored.
+    
+    max_df_ratio: float,
+        Maximum ratio of documents a term can occur in.
+        More frequent words get ignored.
+    
+    Returns
+    =======
+    
+    idf: dict
+        For each term, the dict contains the idf value.
+        
+    """
+    
+    # YOUR CODE HERE
+    idf = {}
+    
+    for word, idx in inv_idx.items():
+        word_docs = len(idx)
+        
+        # Word in too few documents
+        if word_docs < min_df:
+            continue
+        # Word in > 95% docs
+        elif word_docs/n_docs > max_df_ratio:
+            continue
+        else:
+            idf[word] = math.log(n_docs/(1+word_docs), 2)
+    
+    return idf
+
+
+def compute_doc_norms(index, idf, n_docs):
+    """ Precompute the euclidean norm of each document.
+    
+    Arguments
+    =========
+    
+    index: the inverted index as above
+    
+    idf: dict,
+        Precomputed idf values for the terms.
+    
+    n_docs: int,
+        The total number of documents.
+    
+    Returns
+    =======
+    
+    norms: np.array, size: n_docs
+        norms[i] = the norm of document i.
+    """
+    
+    # YOUR CODE HERE
+    norms = np.zeros(n_docs)
+    
+    for word, idx in index.items():
+        for doc_id, tf in idx:
+            # Make sure word has not been pruned
+            if word in idf:
+                norms[doc_id] += (tf*idf[word])** 2
+        
+    return np.sqrt(norms)
+
+
+def index_search(query, index, idf, doc_norms):
+    """ Search the collection of documents for the given query
+    
+    Arguments
+    =========
+    
+    query: string,
+        The query we are looking for.
+    
+    index: an inverted index as above
+    
+    idf: idf values precomputed as above
+    
+    doc_norms: document norms as computed above
+    
+    Returns
+    =======
+    
+    results, list of tuples (score, doc_id)
+        Sorted list of results such that the first element has
+        the highest score, and `doc_id` points to the document
+        with the highest score.
+        
+    """
+    
+    # YOUR CODE HERE         
+    results = np.zeros(len(doc_norms))
+    
+    # Tokenize query
+    tokenizer = TreebankWordTokenizer()
+    q = tokenizer.tokenize(query.lower())
+    q_weights = {}
+    
+    for term in q:
+        postings = []
+        if term not in index.keys():
+            continue
+        else:
+            postings = index[term]
+            
+        for doc_id, tf in postings:
+            wij = tf*idf[term]
+            wiq = q.count(term)*idf[term]
+            q_weights[term] = wiq
+
+            results[doc_id] += wij*wiq
+    
+    # Find query norm
+    q_norm = 0
+    for w in q_weights.values():
+        q_norm += w*w
+    q_norm = math.sqrt(q_norm)
+    
+    # Normalize
+    results = results/(doc_norms*q_norm+1)
+            
+   # change results to (score, doc_id) format   
+    results = [(results[i], i) for i in range(0, len(results))]
+    
+    # sort results by score
+    results.sort()
+
+    return results[::-1]
+
+if __name__ == "__main__":
+	inv_idx = build_inverted_index(all_talks)
+
+	idf = compute_idf(inv_idx, len(all_talks), min_df=10,  max_df_ratio=0.1)
+
+	# prune the terms left out by idf
+	inv_idx = {key: val for key, val in inv_idx.items() if key in idf}
+
+	doc_norms = compute_doc_norms(inv_idx, idf, len(all_talks))
+
+	score, doc_id = index_search(query, inv_idx, idf, doc_norms)
+
 
