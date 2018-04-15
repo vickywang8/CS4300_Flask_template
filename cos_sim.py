75ef2fc1632d6a1dd2e9a357a8ba9dacaf9d60c2
vickywang8 <vickyminzhi@gmail.com>
2018-04-15 13:39:02 -0400

    read in ted_main.csv into a dictionary

--- cos_sim.py
+++ cos_sim.py
@@ -1,7 +1,22 @@
 import numpy as np
 import math
+import csv
 from collections import defaultdict
 from nltk.tokenize import TreebankWordTokenizer
+
+ted_dict = {}
+
+with open('ted_main.csv') as csvfile:
+	reader = csv.DictReader(csvfile)
+	i = 0
+	for row in reader:
+		ted_dict[i] = {"title": row['title'], 
+					   "description": row['description'], 
+					   "speaker": row['main_speaker'], 
+					   "tags": row["tags"], 
+					   "url": row["url"], 
+					   "views": row['views']}
+		i += 1
 
 tokenizer = TreebankWordTokenizer()
 tokenize_blurb = tokenizer.tokenize(blurb)
