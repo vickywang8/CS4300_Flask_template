4e5503a0f293deac2125720602019a317f3316fa
vickywang8 <vickyminzhi@gmail.com>
2018-04-17 19:12:45 -0400

    add quotes around the query

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -161,6 +161,6 @@ def search():
                 if all_talks[doc_id] not in data and len(data) < 5:
                     data.append(all_talks[doc_id])
 
-        output_message = "You searched for " + query
+        output_message = "You searched for \"" + query + "\""
     print(data)
     return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
